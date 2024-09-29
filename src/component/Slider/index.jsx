5d808c2df15f4b368ff018b80efa0b84d4f79092
ThienDinh203 <21522617@gm.uit.edu.vn>
2024-09-29 13:13:56 +0700

    Fix Slider transition

--- src/component/Slider/index.jsx
+++ src/component/Slider/index.jsx
@@ -14,12 +14,6 @@ const settings = {
       },
     },
     {
-      breakpoint: 1024,
-      settings: {
-        slidesToShow: 3,
-      },
-    },
-    {
       breakpoint: 768,
       settings: {
         slidesToShow: 2,
