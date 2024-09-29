4f6c5b1d95d595c9016171a2d8204feaaab4676a
ThienDinh203 <21522617@gm.uit.edu.vn>
2024-09-29 12:13:56 +0700

    Update Slider transition

--- src/component/Slider/index.jsx
+++ src/component/Slider/index.jsx
@@ -14,6 +14,12 @@ const settings = {
       },
     },
     {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+      },
+    },
+    {
       breakpoint: 768,
       settings: {
         slidesToShow: 2,
