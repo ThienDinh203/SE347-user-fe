7b14d7dc246afd4e3c90b5b192742f716918e82d
DuyLe24 <21521996@gm.uit.edu.vn>
2025-01-02 16:00:45 +0700

    author page

--- src/pages/Authors/index.jsx
+++ src/pages/Authors/index.jsx
@@ -1,11 +1,11 @@
 import React, { useEffect, useState, useContext } from "react";
 import { UserContext } from "../../../src/context/UserContext";
-
-
-
 const AuthorsPage = () => {
+
   const [authors, setAuthors] = useState([]);
+
   const [loading, setLoading] = useState(true);
+
   const { user } = useContext(UserContext);
 
   useEffect(() => {
@@ -40,26 +40,32 @@ const AuthorsPage = () => {
         <p>Loading...</p>
       ) : (
         <ul className="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-8">
-          {/* {randomAuthors.map((author) => ( */}
+
           {authors.map((author) => (
+
             <div key={author.id} className="flex flex-col items-center px-4">
               <div className="flex flex-col items-center">
-                <a href={`/tac-gia/${author.id}`}>
-                  <img
-                    className="flex items-center h-48 w-48 rounded-full ring-2 ring-white object-cover"
-                    src={
-                      author.image
-                        ? author.image.includes("/")
-                          ? author.image
-                          : `http://localhost:8080/tg_image/${author.image}`
-                        : "https://bizweb.dktcdn.net/100/363/455/articles/blank-author-33728236-0ca7-4f4e-a265-ddcd14036f53.jpg?v=1705287921247"
-                    }
-                    alt={author.tenTacGia}
-                  />
-                </a>
+                <div>
+                  <a href={`/tac-gia/${author.id}`}>
+                    <img
+                      className="flex items-center h-48 w-48 rounded-full ring-2 ring-white object-cover"
+                      src={
+                        author.image
+                          ? author.image.includes("/")
+                            ? author.image
+                            : `http://localhost:8080/tg_image/${author.image}`
+                          : "https://bizweb.dktcdn.net/100/363/455/articles/blank-author-33728236-0ca7-4f4e-a265-ddcd14036f53.jpg?v=1705287921247"
+                      }
+
+                      alt={author.tenTacGia}
+                    />
+                  </a>
+                </div>
                 <a href={`/tac-gia/${author.id}`}>
                   <h3 className="mt-2 text-base font-medium text-lg text-gray-900">
+
                     {author.tenTacGia}
+
                   </h3>
                 </a>
               </div>
